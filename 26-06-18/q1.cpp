614d1eee7ecac43c8bf0ca01ea1d873ea7ff71a7
Aditya Gupta <34581503+aditya1k2@users.noreply.github.com>
2018-07-26 10:28:05 +0530

    Add files via upload

---
+++ 26-06-18/q1.cpp
@@ -0,0 +1,42 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+	int n,lar=0;
+	printf("Enter The Size Of the Array\n");
+	scanf("%d",&n);
+	int *ptr=(int *)(malloc(n*sizeof(int)));
+	for(int i=0;i<n;i++)
+	{
+		printf("Enter %d element:-\n",i+1);
+		scanf("%d",ptr+i);
+	}
+	printf("**************************\n");
+	for(int i=0;i<n;i++)
+	{
+		printf("%d\n",*(ptr+i));
+		
+	}
+	for(int i=0;i<n;i++)
+	{
+		if(*(ptr+i)>lar)
+		{
+			lar=*(ptr+i);
+		}
+	}
+	printf("**********************************\n");
+	printf("Largest Number is:-%d\n",lar);
+	lar=*ptr;			
+	printf("**********************************\n");
+	for(int i=0;i<n;i++)
+	{
+		if(*(ptr+i)<=lar)
+		{
+			lar=*(ptr+i);
+		}
+	}
+	printf("Smallest Number is:-%d\n",lar);
+	
+	return 0;
+}
+	
---
+++ 26-06-18/q2.cpp
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+	int n,temp;
+	printf("Enter The Size OFthe array\n");
+	scanf("%d",&n);
+	int *ptr=(int*)malloc(n*sizeof(int));
+	int *ptr1=(int*)malloc(n*sizeof(int));
+	for(int i=0;i<n;i++)
+	{
+		printf("Enter The %d element of the  array\n",i+1);
+		scanf("%d",ptr+i);
+	}
+	for(int i=n-1,j=0;i>=0;i--,j++)
+	{
+		*(ptr1+j)=*(ptr+i);
+	}
+	printf("Elements Are:-\n");
+	for(int i=0;i<n;i++)
+	{
+		printf("%d\n",*(ptr1+i));
+	
+	}	
+	
+		
+		
+	return 0;
+}
